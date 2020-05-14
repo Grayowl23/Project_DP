dc794583e755f8dd41f718ec7ee9a9936648ba6a
Grayowl23 <annixowl@gmail.com>
2020-05-14 14:13:12 +0300

    Update builder.cpp

--- builder.cpp
+++ builder.cpp
@@ -4,7 +4,7 @@
 *   www.algorytm.org                    *
 *   Opracowal Dworak Kamil              *
 *****************************************/
- 
+#include "stdafx.h"
 #include <iostream>
 #include <string>
 using namespace std;
